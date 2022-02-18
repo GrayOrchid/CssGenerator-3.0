e3ac4ae3d479160489d804e61c534bde4e96b549
GrayOrchid <jorajostart@bk.ru>
2022-02-18 20:10:58 +0300

    add Reset
    add Reset

--- CssGenerator/i.js
+++ CssGenerator/i.js
@@ -1,12 +1,33 @@
+//Табы
+
 let tabButton = document.querySelectorAll('.tab-button')
 let tabsItem = document.querySelectorAll('.tabs-item')
-let box = document.querySelector('.box')
-let resultText = document.querySelector('.resultText')
-let burgerMenu = document.querySelector('.burgerMenu')
+
+
+//Бургер Меню 
 let open = document.querySelector('.open')
 let close = document.querySelector('.close')
 let burgerCont = document.querySelector('.burgerCont')
+let burgerMenu = document.querySelector('.burgerMenu')
 
+
+
+// Результать 
+let $border = document.querySelector('.border')
+let $borderRadius = document.querySelector('.borderRadius')
+let $gradient = document.querySelector('.gradient')
+let $shadowText = document.querySelector('.shadowText')
+let $TextShadow = document.querySelector('.TextShadow')
+let $shadowBox = document.querySelector('.shadowBox')
+let $transform = document.querySelector('.transform')
+
+// Тест бокс
+let $box = document.querySelector('.box')
+
+// Кнопка сбрасывание 
+let reset = document.querySelector('.reset')
+
+// Функции Бургер Меню 
 burgerMenu.addEventListener('click',()=>{
 	burgerMenu.classList.remove('burgerAcitve')
 })
@@ -15,20 +36,16 @@ burgerCont.addEventListener('click',(e)=>{
 })
 open.addEventListener('click', ()=>{
 	burgerMenu.classList.add('burgerAcitve')
-
-
 })
 close.addEventListener('click',()=>{
 	burgerMenu.classList.remove('burgerAcitve')
 
 })
 
-
-
+// Функция Табы
 tabButton.forEach(function (e) {
 	e.addEventListener('click', function () {
 		let currentBtn = e
-	
 		let tabId = currentBtn.getAttribute('data-tab')
 		let currentTab = document.querySelector(tabId)
 		console.log(tabId);
@@ -38,34 +55,49 @@ tabButton.forEach(function (e) {
 		tabsItem.forEach(function (e) {
 			e.classList.remove('active')
 		})
-
 		currentBtn.classList.add('btb')
 		currentTab.classList.add('active')
 	})
+})
+
+// Сбрасывание 
+reset.addEventListener('click',()=>{
+	$box.style.border = `0px solid black`
+	$border.textContent = `border: `
+
+	$box.style.borderRadius= '0px 0px 0px 0px '
+	$borderRadius.textContent = 'border-radius:'
+
+	$TextShadow.style.textShadow  = '0px 0px 0px '
+	$shadowText.textContent = 'text-shadow: '
+	
+	$box.style.boxShadow = '0px 0px 0px '
+	$shadowBox.textContent = 'box-shadow'
+
+	$box.style.background = 'steelblue'
+	$gradient.textContent = 'gradient:'
+
+	$box.style.transform =` scale(1) rotate(0deg) translate(0px, 0px) skew(0deg, 0deg)`
+	$transform.textContent = 'transform:'
 })
 
 function border(params) {
 	let borderSize = document.querySelector('.borderSize').value
 	let borderType = document.querySelector('.borderType').value
 	let colorBorder = document.querySelector('.colorBorder').value
-	console.log(borderSize.textContent)	;
-    let $border = document.querySelector('.border')
 	if (borderSize > 50) {
 		borderSize = 50
 	}
-	box.style.border = `${borderSize}px ${borderType} ${colorBorder} `
+	$box.style.border = `${borderSize}px ${borderType} ${colorBorder} `
 	$border.textContent = `border: ${borderSize}px ${borderType} ${colorBorder}`
 }
 
 function borderRadius(params) {
-
-
 	let rtl = document.querySelector(".rtl").value;
 	let rtr = document.querySelector(".rtr").value;
 	let rbr = document.querySelector(".rbr").value;
 	let rbl = document.querySelector(".rbl").value;
-    let $borderRadius = document.querySelector('.borderRadius')
-	box.style.borderRadius = `${rtl}px ${rtr}px ${rbr}px ${rbl}px `
+	$box.style.borderRadius = `${rtl}px ${rtr}px ${rbr}px ${rbl}px `
 	$borderRadius.textContent = `border-radius:${rtl}px ${rtr}px ${rbr}px ${rbl}px; `
 }
 
@@ -74,11 +106,9 @@ function textShadow(params) {
 	let ShadowTextY = document.querySelector('.ShadowTextY').value
 	let ShadowTextBlur = document.querySelector('.ShadowTextBlur').value
 	let ShadowTextColor = document.querySelector('.ShadowTextColor').value
-	let shadowText = document.querySelector('.shadowText')
-	let TextShadow = document.querySelector('.TextShadow')
-	TextShadow.style.textShadow = `${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor}`
-	shadowText.textContent = `text-shadow: ${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor} `
-	console.log(`${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor}`);
+	$TextShadow.style.textShadow = `${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor}`
+	$shadowText.textContent = `text-shadow: ${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor} `
+
 }
 
 function boxShadow(params) {
@@ -87,9 +117,8 @@ function boxShadow(params) {
 	let ShadowBoxBlur = document.querySelector('.ShadowBoxBlur').value
 	let ShadowBoxColor = document.querySelector('.ShadowBoxColor').value
 	let boxShadowType = document.querySelector('.boxShadowType').value
-	let shadowBox = document.querySelector('.shadowBox')
-	box.style.boxShadow = ` ${boxShadowType} ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
-	shadowBox.textContent = `box-shadow: ${boxShadowType}  ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
+	$box.style.boxShadow = ` ${boxShadowType} ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
+	$shadowBox.textContent = `box-shadow: ${boxShadowType}  ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
 }
 
 function gradient(params) {
@@ -101,13 +130,12 @@ function gradient(params) {
 	let gradientThirdColor = document.querySelector('.gradientThirdColor').value
 	let gradientThirdPoint = document.querySelector('.gradientThirdPoint').value
 	let gradientType = document.querySelector('.gradientType').value
-let gradient = document.querySelector('.gradient')
 	if (gradientType === 'radial') {
-		box.style.background = `radial-gradient(circle, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
-		gradient.textContent = `background : radial-gradient(circle, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
+		$box.style.background = `radial-gradient(circle, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
+		$gradient.textContent = `background : radial-gradient(circle, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
 	} else {
-		box.style.background = `linear-gradient(${gradientDeg}deg, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
-		gradient.textContent = `background : linear-gradient(${gradientDeg}deg, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
+		$box.style.background = `linear-gradient(${gradientDeg}deg, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
+		$gradient.textContent = `background : linear-gradient(${gradientDeg}deg, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
 	}
 }
 function transform(params) {
@@ -117,14 +145,8 @@ function transform(params) {
     let translateY = document.querySelector('.translateY').value
     let skewX = document.querySelector('.skewX').value
     let skewY = document.querySelector('.skewY').value
-	let transform = document.querySelector('.transform')
-    transform.textContent = `transform: scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
-    box.style.transform =` scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
+    $transform.textContent = `transform: scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
+    $box.style.transform =` scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
 }
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-let vasya = { name: "Вася", age: 25 };
-let arr = [ petya,vasya,  masha  ];
-console.log(`arr`,arr);
-let q = arr.sort((a,b)=>a.age - b.age)
+
 
