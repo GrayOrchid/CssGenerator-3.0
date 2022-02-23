d97d508ddade206f6fbf24456a4ba06a3de8c2d4
GrayOrchid <jorajostart@bk.ru>
2022-02-23 15:45:38 +0300

    udpate css / change class names.

--- CssGenerator/i.js
+++ CssGenerator/i.js
@@ -5,44 +5,44 @@ let tabsItem = document.querySelectorAll('.tabs-item')
 //Бургер Меню 
 let open = document.querySelector('.open')
 let close = document.querySelector('.close')
-let burgerCont = document.querySelector('.burgerCont')
-let burgerMenu = document.querySelector('.burgerMenu')
+let burgerCont = document.querySelector('.burger-cont')
+let burgerMenu = document.querySelector('.burger-menu')
 let $body = document.querySelector('body')
 
 // Результать 
 let $border = document.querySelector('.border')
-let $borderRadius = document.querySelector('.borderRadius')
+let $borderRadius = document.querySelector('.border-radius')
 let $gradient = document.querySelector('.gradient')
-let $shadowText = document.querySelector('.shadowText')
-let $TextShadow = document.querySelector('.TextShadow')
-let $shadowBox = document.querySelector('.shadowBox')
+let $shadowText = document.querySelector('.shadow-text')
+let $TextShadow = document.querySelector('.text-shadow')
+let $shadowBox = document.querySelector('.shadow-box')
 let $transform = document.querySelector('.transform')
 let $transition = document.querySelector('.transition')
-let $hoverButton = document.querySelector('.hoverButton')
+let $hoverButton = document.querySelector('.hover-button')
 let $bacdropFilter = document.querySelector('.backdrop-filter')
 
 
 // Тест бокс
 let $box = document.querySelector('.box')
-let $boxTest = document.querySelector('.boxTest')
+let $boxTest = document.querySelector('.box-test')
 // Кнопка сбрасывание 
 let reset = document.querySelector('.reset')
 
 // Функции Бургер Меню 
 burgerMenu.addEventListener('click',()=>{
-	burgerMenu.classList.remove('burgerAcitve')
-	$body.classList.remove('blockScrool')
+	burgerMenu.classList.remove('burger-acitve')
+	$body.classList.remove('block-scrool')
 })
 burgerCont.addEventListener('click',(e)=>{
 	e.stopPropagation();
 })
 open.addEventListener('click', ()=>{
-	burgerMenu.classList.add('burgerAcitve')
-	$body.classList.add('blockScrool')
+	burgerMenu.classList.add('burger-acitve')
+	$body.classList.add('block-scrool')
 })
 close.addEventListener('click',()=>{
-	burgerMenu.classList.remove('burgerAcitve')
-	$body.classList.remove('blockScrool')
+	burgerMenu.classList.remove('burger-acitve')
+	$body.classList.remove('block-scrool')
 })
 
 // Функция Табов
@@ -87,9 +87,9 @@ reset.addEventListener('click',()=>{
 })
 
 function border(params) {
-	let borderSize = document.querySelector('.borderSize').value
-	let borderType = document.querySelector('.borderType').value
-	let colorBorder = document.querySelector('.colorBorder').value
+	let borderSize = document.querySelector('.border-size').value
+	let borderType = document.querySelector('.border-type').value
+	let colorBorder = document.querySelector('.color-border').value
 	
 	if (borderSize > 50) {
 		borderSize = 50
@@ -109,35 +109,35 @@ function borderRadius(params) {
 }
 
 function textShadow(params) {
-	let ShadowTextX = document.querySelector('.ShadowTextX').value
-	let ShadowTextY = document.querySelector('.ShadowTextY').value
-	let ShadowTextBlur = document.querySelector('.ShadowTextBlur').value
-	let ShadowTextColor = document.querySelector('.ShadowTextColor').value
+	let ShadowTextX = document.querySelector('.shadow-text-X').value
+	let ShadowTextY = document.querySelector('.shadow-text-Y').value
+	let ShadowTextBlur = document.querySelector('.shadow-text-blur').value
+	let ShadowTextColor = document.querySelector('.shadow-text-color').value
 
 	$TextShadow.style.textShadow = `${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor}`
 	$shadowText.textContent = `text-shadow: ${ShadowTextX}px ${ShadowTextY}px ${ShadowTextBlur}px ${ShadowTextColor} `
 }
 
 function boxShadow(params) {
-	let ShadowBoxX = document.querySelector('.ShadowBoxX').value
-	let ShadowBoxY = document.querySelector('.ShadowBoxY').value
-	let ShadowBoxBlur = document.querySelector('.ShadowBoxBlur').value
-	let ShadowBoxColor = document.querySelector('.ShadowBoxColor').value
-	let boxShadowType = document.querySelector('.boxShadowType').value
+	let ShadowBoxX = document.querySelector('.shadow-box-X').value
+	let ShadowBoxY = document.querySelector('.shadow-box-Y').value
+	let ShadowBoxBlur = document.querySelector('.shadow-box-blur').value
+	let ShadowBoxColor = document.querySelector('.shadow-box-color').value
+	let boxShadowType = document.querySelector('.box-shadow-type').value
 
 	$box.style.boxShadow = ` ${boxShadowType} ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
 	$shadowBox.textContent = `box-shadow: ${boxShadowType}  ${ShadowBoxX}px ${ShadowBoxY}px ${ShadowBoxBlur}px ${ShadowBoxColor } `
 }
 
 function gradient(params) {
-	let gradientDeg = document.querySelector('.gradientDeg').value
-	let gradientFirstColor = document.querySelector('.gradientFirstColor').value
-	let gradientFirstPoint = document.querySelector('.gradientFirstPoint').value
-	let gradientSecondColor = document.querySelector('.gradientSecondColor').value
-	let gradientSecondPoint = document.querySelector('.gradientSecondPoint').value
-	let gradientThirdColor = document.querySelector('.gradientThirdColor').value
-	let gradientThirdPoint = document.querySelector('.gradientThirdPoint').value
-	let gradientType = document.querySelector('.gradientType').value
+	let gradientDeg = document.querySelector('.gradient-deg').value
+	let gradientFirstColor = document.querySelector('.gradient-first-color').value
+	let gradientFirstPoint = document.querySelector('.gradient-first-point').value
+	let gradientSecondColor = document.querySelector('.gradient-second-color').value
+	let gradientSecondPoint = document.querySelector('.gradient-second-point').value
+	let gradientThirdColor = document.querySelector('.gradient-third-color').value
+	let gradientThirdPoint = document.querySelector('.gradient-third-point').value
+	let gradientType = document.querySelector('.gradient-type').value
 
 	if (gradientType === 'radial') {
 		$box.style.background = `radial-gradient(circle, ${gradientFirstColor} ${gradientFirstPoint}%, ${gradientSecondColor} ${gradientSecondPoint}%, ${gradientThirdColor} ${gradientThirdPoint}%)`
@@ -150,10 +150,10 @@ function gradient(params) {
 function transform(params) {
     let scale = document.querySelector('.scale').value/ 10
     let rotate= document.querySelector('.rotate').value
-    let translateX = document.querySelector('.translateX').value
-    let translateY = document.querySelector('.translateY').value
-    let skewX = document.querySelector('.skewX').value
-    let skewY = document.querySelector('.skewY').value
+    let translateX = document.querySelector('.translate-X').value
+    let translateY = document.querySelector('.translate-Y').value
+    let skewX = document.querySelector('.skew-X').value
+    let skewY = document.querySelector('.skew-Y').value
 
     $transform.textContent = `transform: scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
     $box.style.transform =` scale(${scale}) rotate(${rotate}deg) translate(${translateX}px, ${translateY}px) skew(${skewX}deg, ${skewY}deg)`
@@ -161,16 +161,9 @@ function transform(params) {
 
 
 function transition(params) {
-	let transitionTime = document.querySelector('.transitionTime').value
+	let transitionTime = document.querySelector('.transition-time').value
 
 	$hoverButton.style.transition = `${transitionTime}s`
 	$transition.textContent = `transition: ${transitionTime}s`
 }
 
-function backdropFilter(params) {
-	let blur = document.querySelector('.blur').value
-	let resulq = document.querySelector('.result')
-let BackdropFilter = documentq.querySelector('.BackdropFilter')
-BackdropFilter.style.backdropFilter = `blur(${blur}px)`
-
-}
